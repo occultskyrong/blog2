89cd6e1e91c4ffee60bba9b240c4ee3272a1e729
ZhangRZ <occultskyrong@163.com>
2018-11-30 18:30:40 +0800

    feat(config): update config & add git ignore config

--- config/index.js
+++ config/index.js
@@ -3,8 +3,12 @@ const development = require('./development');
 
 const ENV = process.env.NODE_ENV || 'development';
 
-const config = {
+const envConfig = {
   development,
 };
 
-module.exports = config[ENV];
+const config = envConfig[ENV];
+
+Object.assign(config, require('./_secretKey')); // 合并秘钥；FIXME:替换为 './secretKey'即可
+
+module.exports = config;
